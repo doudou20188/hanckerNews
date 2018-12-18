73fe7821f582ab5f497662626262d2ee7f274493
yangtao <yangtao@qq.com>
2018-12-18 18:44:56 +0800

    the first push

---
+++ harckernews/src/main/java/com/com/cskaoyan/newsproject/NewsprojectApplication.java
@@ -0,0 +1,14 @@
+package com.com.cskaoyan.newsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsprojectApplication.class, args);
+    }
+
+}
+
